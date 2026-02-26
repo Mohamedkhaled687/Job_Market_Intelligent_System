e1e01ca6ae5054c32a2ec334d14d731b7b140382
Mohamedkhaled687 <mohamedkhaled47526@gmail.com>
2026-02-26 23:51:14 +0200

    add the __init__.py files for the models

---
+++ src/models/__init__.py
@@ -0,0 +1,3 @@
+from .database import connect_db, close_db, get_db
+from .schemas import *
+from .enums import *
---
+++ src/models/schemas/__init__.py
@@ -0,0 +1,13 @@
+from .job import JobBase, JobCreate, JobEnrichment, JobInDB, JobResponse, PaginatedJobsResponse
+from .crawl_log import CrawlLogCreate, CrawlLogResponse
+
+__all__ = [
+    "JobBase",
+    "JobCreate",
+    "JobEnrichment",
+    "JobInDB",
+    "JobResponse",
+    "PaginatedJobsResponse",
+    "CrawlLogCreate",
+    "CrawlLogResponse",
+]
